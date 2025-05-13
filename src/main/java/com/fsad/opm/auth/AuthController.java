21420f5140f5c0273e5a3f6b0d47768f2f0a29b2
Sachin T P <sachin.t.p@sap.com>
2025-05-14 00:21:45 +0530

    Registration and Login rough code with JWT implemented

---
+++ src/main/java/com/fsad/opm/auth/AuthController.java
@@ -0,0 +1,26 @@
+package com.fsad.opm.auth;
+
+import com.fsad.opm.dto.AuthRequest;
+import com.fsad.opm.dto.AuthResponse;
+import com.fsad.opm.dto.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+}
---
+++ src/main/java/com/fsad/opm/dto/RegisterRequest.java
@@ -0,0 +1,13 @@
+package com.fsad.opm.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequest {
+    private String username;
+    private String email;
+    private String password;
+}
