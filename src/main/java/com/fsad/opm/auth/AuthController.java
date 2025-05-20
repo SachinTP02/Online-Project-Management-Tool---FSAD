2cc332df956f976188de34e2c123aaa68aab5b04
soujanya0924 <soujanyapracticing@gmail.com>
2025-05-20 16:58:52 +0530

    approval api updated

--- src/main/java/com/fsad/opm/auth/AuthController.java
+++ src/main/java/com/fsad/opm/auth/AuthController.java
@@ -3,6 +3,7 @@ package com.fsad.opm.auth;
 import com.fsad.opm.dto.AuthRequest;
 import com.fsad.opm.dto.AuthResponse;
 import com.fsad.opm.dto.RegisterRequest;
+import com.fsad.opm.dto.StatusUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +22,19 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest request) {
+
         return ResponseEntity.ok(authService.authenticate(request));
     }
+
+    @GetMapping("/pending-users")
+    public ResponseEntity<?> getPendingUsers(@RequestHeader("Authorization") String token) {
+        return ResponseEntity.ok(authService.getPendingUsers(token));
+    }
+
+    @PutMapping("/statusUpdate")
+    public ResponseEntity<?> updateUserStatus(@RequestHeader("Authorization") String token,
+                                              @RequestBody StatusUpdateRequest request) {
+        return ResponseEntity.ok(authService.updateUserStatus(token, request));
+    }
+
 }
