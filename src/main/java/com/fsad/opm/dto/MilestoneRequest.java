df31aab1afa68cc952e5352f016a01c12f540497
Sachin T P <sachin.t.p@sap.com>
2025-05-21 13:49:05 +0530

    updated bruno ans milestone request

--- src/main/java/com/fsad/opm/dto/MilestoneRequest.java
+++ src/main/java/com/fsad/opm/dto/MilestoneRequest.java
@@ -7,7 +7,6 @@ import java.time.LocalDate;
 @Data
 public class MilestoneRequest {
     private String name;
-    private String description;
     private LocalDate startDate;
     private LocalDate endDate;
 }
