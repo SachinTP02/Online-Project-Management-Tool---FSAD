34985d148b8c73bd0232a2e3fbc9d5d7a8d67f93
Sachin T P <sachin.t.p@sap.com>
2025-05-30 18:03:53 +0530

    task attachment repository

--- server/src/main/java/com/fsad/opm/repository/TaskAttachmentRepository.java
+++ server/src/main/java/com/fsad/opm/repository/TaskAttachmentRepository.java
@@ -1,6 +1,7 @@
 package com.fsad.opm.repository;
 
 import com.fsad.opm.model.Project;
+import com.fsad.opm.model.TaskAttachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
